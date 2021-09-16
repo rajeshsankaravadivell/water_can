7e16fe52b5d89505d3defead858cc31a10d6799e
rampowiz <rajeshsankaravadivell@gmail.com>
2021-09-16 11:45:54 +0530

    first commit

---
+++ lib/screens/cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Cart extends StatelessWidget {
+  const Cart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Center(
+
+        child: Text('cart'),
+
+      ),
+
+    );
+  }
+}
